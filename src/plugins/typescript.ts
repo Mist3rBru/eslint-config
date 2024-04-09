cddf03e7039019f9c183e39fa241cbff2a6c1d28
Bruno Rocha <brunorocha2674@gmail.com>
2024-04-09 10:10:27 -0300

    feat(plugins/typescript): config @typescript-eslint/restrict-plus-operands

--- src/plugins/typescript.ts
+++ src/plugins/typescript.ts
@@ -409,6 +409,12 @@ export const typescriptPlugin = definePlugin({
 
     // https://typescript-eslint.io/rules/use-unknown-in-catch-callback-variable
     '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
+
+    // https://typescript-eslint.io/rules/restrict-plus-operands
+    '@typescript-eslint/restrict-plus-operands': [
+      'error',
+      { allowNumberAndString: true },
+    ],
   },
   testRules: {
     // https://typescript-eslint.io/rules/no-explicit-any
