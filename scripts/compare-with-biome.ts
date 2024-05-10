fe16db30a09d0ce27c1030b4500cca79637fef2d
Bruno Rocha <brunorocha2674@gmail.com>
2024-05-10 17:52:22 -0300

    fix: script

--- scripts/compare-with-biome.ts
+++ scripts/compare-with-biome.ts
@@ -2,14 +2,13 @@
 
 /* eslint-disable */
 import type { EslintPlugin } from '../src/types.js'
+import { toCamelCase } from '../src/utils/mappers.js'
 import { readdirSync } from 'node:fs'
 import path from 'node:path'
 import puppeteer from 'puppeteer'
 
 ;(async (): Promise<void> => {
-  const browser = await puppeteer.launch({
-    product: 'firefox',
-  })
+  const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(
     'https://github.com/biomejs/biome/discussions/3#discussioncomment-9381916'
@@ -23,9 +22,17 @@ import puppeteer from 'puppeteer'
       const cells = row.querySelectorAll('td')
 
       // Extract text from Eslint and Biome columns
-      const eslint = cells[0].textContent.trim()
-      const biome = cells[2].textContent.trim()
+      const eslint = cells[0]?.textContent.trim()
+      const biome = cells[2]?.textContent.trim()
 
+      if (
+        eslint?.split(' ').length > 1 ||
+        eslint?.includes('\n') ||
+        biome?.split(' ').length > 1 ||
+        biome?.includes('\n')
+      ) {
+        continue
+      }
       // Push data to array
       data.push({ eslint, biome })
     }
@@ -33,30 +40,38 @@ import puppeteer from 'puppeteer'
     return data
   })
 
+  console.log(biomeEquivalents)
+
   const pluginsPath = path.resolve('src/plugins')
   const pluginFiles = readdirSync(pluginsPath).filter(
     fileName => !fileName.endsWith('.spec.ts')
   )
+  let totalCounter = 0
 
   for (const pluginFile of pluginFiles) {
-    const plugin = (await import(
-      path.resolve(pluginsPath, pluginFile)
-    )) as unknown as EslintPlugin
+    const plugin = (await import(path.resolve(pluginsPath, pluginFile)))[
+      `${toCamelCase(pluginFile.replace('.ts', ''))}Plugin`
+    ] as unknown as EslintPlugin
     const pluginRules = Object.entries(plugin.rules)
     const pluginActiveRules = (
       pluginRules.length > 0 ? pluginRules : Object.entries(plugin.testRules)
     ).filter(([_, ruleEntry]) => ruleEntry !== 'off')
+    let pluginCounter = 0
 
     for (const [ruleName] of pluginActiveRules) {
       const biomeEquivalent = biomeEquivalents.find(data =>
         ruleName.includes(data.eslint)
       )
 
-      process.stdout.write(
-        `${plugin.name}: ${ruleName} | ${biomeEquivalent?.biome}`
-      )
+      if (!biomeEquivalent?.biome) {
+        process.stdout.write(`${plugin.name}: ${ruleName}\n`)
+        pluginCounter++
+      }
     }
+    totalCounter += pluginCounter
+    process.stdout.write(`${plugin.name}: TOTAL: ${pluginCounter}\n\n`)
   }
+  process.stdout.write(`TOTAL: ${totalCounter}\n\n`)
 
   await browser.close()
 })()
